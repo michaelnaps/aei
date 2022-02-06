f1958ec03f0bfae4aebd8cf766ec50ec826a9bfb
michaelnaps <napoli.38@osu.edu>
2022-02-06 12:50:08 -0500

    created JS testing prog/circuit, successful

---
+++ joystickTesting_nano/joystickTesting_nano.ino
@@ -0,0 +1,46 @@
+int valX = 0;
+int valY = 0;
+
+const int ledX = 10;
+const int ledY = 9;
+
+const int jsX = A0;
+const int jsY = A1;
+
+void setup() {
+  pinMode(ledX, OUTPUT);
+  pinMode(ledY, OUTPUT);
+
+  pinMode(jsX, INPUT);
+  pinMode(jsY, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  const int mjs = 600;
+  const int tol = 100;
+
+  int newX = analogRead(jsX);
+  int newY = analogRead(jsY);
+
+  if (newX > (mjs + tol) && valX < 255) {
+    valX = valX + 1;
+  }
+  else if (newX < (mjs - tol) && valX > 0) {
+    valX = valX - 1;
+  }
+
+  if (newY > (mjs + tol) && valY < 255) {
+    valY = valY + 1;
+  }
+  else if (newY < (mjs - tol) && valY > 0) {
+    valY = valY - 1;
+  }
+
+  analogWrite(ledX, valX);
+  analogWrite(ledY, valY);
+
+  Serial.print(valX); Serial.print(", "); Serial.print(valY); Serial.print(" | ");
+  Serial.print(newX); Serial.print(", "); Serial.println(newY);
+}
