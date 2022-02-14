6957e0897d91de86f48b467ecd880404b7b8f07c
Michael Napoli <napoli.38@osu.edu>
2022-02-14 17:20:56 -0500

    transistor testing with 2-D TCP setup

--- transistorTesting_nano/transistorTesting_nano.ino
+++ transistorTesting_nano/transistorTesting_nano.ino
@@ -1,18 +1,46 @@
+int valX = 0;
+int valY = 0;
+
 const int trn1 = 10;
-// const int trn2 = 9;
+const int trn2 = 9;
+
+const int jsX = A0;
+const int jsY = A1;
 
 void setup() {
   pinMode(trn1, OUTPUT);
-  // pinMode(trn2, OUTPUT);
+  pinMode(trn2, OUTPUT);
+
+  pinMode(jsX, INPUT);
+  pinMode(jsY, INPUT);
+  
   Serial.begin(9600);
 }
 
 void loop() {
-  for (int i(0); i < 255; i += 5) {
-    analogWrite(trn1, 255/2);
-    // analogWrite(trn2, i);
-    Serial.println(i);
+  const int mjs = 350;
+  const int tol = 100;
 
-    delay(10000);
+  int newX = analogRead(jsX);
+  int newY = analogRead(jsY);
+
+  if (newX > (mjs + tol) && valX < 255) {
+    valX = valX + 1;
   }
+  else if (newX < (mjs - tol) && valX > 0) {
+    valX = valX - 1;
+  }
+
+  if (newY > (mjs + tol) && valY < 255) {
+    valY = valY + 1;
+  }
+  else if (newY < (mjs - tol) && valY > 0) {
+    valY = valY - 1;
+  }
+
+  analogWrite(trn1, valX);
+  analogWrite(trn2, valY);
+  
+  Serial.print(valX); Serial.print(", "); Serial.print(valY); Serial.print(" | ");
+  Serial.print(newX); Serial.print(", "); Serial.println(newY);
 }
