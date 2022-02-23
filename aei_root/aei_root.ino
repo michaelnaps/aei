6e67d51857edf4629109c8018f4dc98fe379e523
Michael Napoli <napoli.38@osu.edu>
2022-02-23 18:40:51 -0500

    tested root sketch with TCP

--- aei_root/aei_root.ino
+++ aei_root/aei_root.ino
@@ -27,7 +27,7 @@ const int TCP[] = {5, 6, 9, 10};
 int TCP_val[4] = {0};
 
 // constant loop variables for TCP
-const int TCP_MAX = 0.42*255;
+const int TCP_MAX = 0.30*255;
 const int adj[] = {1, -1, 1, -1};
 
 
